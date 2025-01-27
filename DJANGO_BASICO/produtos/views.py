3f05a5914ec9374efef71fbccbd818c5921b24d6
Daniel Steinbruch Pereira <63525470010@senacrs.edu.br>
2025-01-27 17:37:11 -0300

    aula

--- DJANGO_BASICO/produtos/views.py
+++ DJANGO_BASICO/produtos/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
+from .models import Pessoa
 
 # Create your views here.
 def ver_produto(request):
-    nome = 'Daniel Steinbruch Pere'
-    return  render(request, 'ver_produto.html',{'nome': nome })
+    if request.method == 'GET':
+        nome = 'Daniel Steinbruch Pere'
+        return  render(request, 'ver_produto.html',{'nome': nome })
+    elif request.method == 'POST':
+        return HttpResponse(f'Nome: {request.POST.get("nome")} <br> Idade: {request.POST.get("idade")}')
 
-def inserir_produto(request):
+#def inserir_produto(request):
     return  render(request, 'inserir_produto.html')
